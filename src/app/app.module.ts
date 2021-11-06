ee0f904e4112e0c500402890ae736e61efc51513
Fabian Miguel <jekoy99@gmail.com>
2021-11-06 22:06:51 +0800

    Created SignUp Screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginScreenComponent } from './Templates/login/login-screen.component';
 import { LoginFormComponent } from './Molecules/login-form/login-form.component';
 import { FormsModule } from '@angular/forms';
+import { SignupScreenComponent } from './Templates/signup-screen/signup-screen.component';
+import { SignupFormComponent } from './Molecules/signup-form/signup-form.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginScreenComponent, LoginFormComponent],
+  declarations: [AppComponent, LoginScreenComponent, LoginFormComponent, SignupScreenComponent, SignupFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

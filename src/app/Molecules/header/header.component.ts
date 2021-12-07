12908560abd2519e0dbbd95e4336702c87fe59ac
Christian Lastimosa <christian.lastimosa.21@usjr.edu.ph>
2021-12-07 23:32:57 +0800

    created header

---
+++ src/app/Molecules/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  userName = 'jorjBugwak';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,19 @@ import { LoginFormComponent } from './Molecules/login-form/login-form.component'
 import { FormsModule } from '@angular/forms';
 import { SignupScreenComponent } from './Templates/signup-screen/signup-screen.component';
 import { SignupFormComponent } from './Molecules/signup-form/signup-form.component';
+import { HomeScreenComponent } from './Templates/home-screen/home-screen.component';
+import { HeaderComponent } from './Molecules/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginScreenComponent, LoginFormComponent, SignupScreenComponent, SignupFormComponent],
+  declarations: [
+    AppComponent,
+    LoginScreenComponent,
+    LoginFormComponent,
+    SignupScreenComponent,
+    SignupFormComponent,
+    HomeScreenComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
